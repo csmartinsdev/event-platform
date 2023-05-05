cd6eb518c761f083567e456b9555a03639bf348d
csmartinsdev <cmartins.dev@gmail.com>
2023-05-05 11:53:26 -0300

    Original released

---
+++ readme.md
@@ -0,0 +1,11 @@
+## INSTALL DEPENDENCIES
+For install all dependencies, you should need to open the terminal and then run the following:
+```bash
+npm install
+```
+## RUN APPLICATION IN DEV ENVIRONMENT
+After installing the dependencies you should run the following command to raise the application:
+```bash
+npm run dev
+```
+
---
+++ src/pages/Event.tsx
@@ -0,0 +1,22 @@
+import { useParams } from "react-router-dom";
+import { Header } from "../components/Header/Header";
+import { Sidebar } from "../components/Sidebar/Sidebar";
+import { Video } from "../components/Video/Video";
+
+export function Event() {
+    const { slug } = useParams<{ slug: string }>()
+
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex flex-1">
+                {
+                    slug 
+                    ? <Video lessonSlug={slug} /> 
+                    : <div className="flex-1"/>
+                }
+                <Sidebar />
+            </main>
+        </div>
+    )
+}
